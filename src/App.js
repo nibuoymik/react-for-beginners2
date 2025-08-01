ab44651c67c0f926c0f9e1683915515a2789e31e
nibuoymik <novena72@naver.com>
2025-08-01 18:16:40 +0900

    01 study

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [coin, setSelectCoin] = useState([]);
+  const onChangeMoney = (event) => setMoney(event.target.value);
+  const onChangeCoin = (event) => setSelectCoin(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
+    if (money === "") {
       return;
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
   };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChangeCoin} value={coin.id}>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol} : ${coin.quotes.USD.price} USD)
+            </option>
+          ))}
+        </select>
+      )}
+      <h2>Your Money input</h2>
       <form onSubmit={onSubmit}>
         <input
-          onChange={onChange}
-          value={toDo}
+          onChange={onChangeMoney}
+          value={money}
           type="text"
-          placeholder="Write your to do..."
+          placeholder="USD"
         />
-        <button>Add To Do</button>
+        <button>Change</button>
       </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
     </div>
   );
 }
